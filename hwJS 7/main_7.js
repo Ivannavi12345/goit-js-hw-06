20955879cfee2b171914ed5ab68beefb8433d3f9
Ivannavi12345 <117343589+Ivannavi12345@users.noreply.github.com>
2023-01-22 20:12:48 +0200

    Delete hwJS 7 directory

--- hwJS 7/main_7.js
+++
@@ -1,8 +0,0 @@
-const textEl = document.getElementById("text");
-const fontEl = document.getElementById("font-size-control");
-
-fontEl.addEventListener("input", (e) => {
-  const size = fontEl.value;
-
-  textEl.style.fontSize = size + "px";
-});
